057f83c95f2e990b11741c439643b64de79373b2
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2022-07-31 09:51:38 +0000

    メソッドの作成とメソッドの呼び出し

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CheckDigit } from './checkDigit';
 
 function App() {
+  const CheckDigitElement = () => {
+    const cd = new CheckDigit
+    const mod10 = cd.mod10()
+
+    return (
+      <p>{ mod10 }</p>
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CheckDigitElement />
     </div>
   );
 }
