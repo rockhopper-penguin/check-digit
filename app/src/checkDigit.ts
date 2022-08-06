8ca177839f8c98037ce4c7e82852ac7863210846
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2022-08-06 12:25:21 +0000

    モジュラス10 ウエイト2・1分割処理の追加

--- app/src/checkDigit.ts
+++ app/src/checkDigit.ts
@@ -1,5 +1,9 @@
+import { Mod10 } from "./calcFunc/mod10"
+
 export class CheckDigit {
     mod10 = () => {
+        const mod10 = new Mod10
+        return mod10.mod10w2Split(589325)
         return 'モジュラス10'
     }
 }
