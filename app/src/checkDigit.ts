2121549ca302f2efe7e714640be488f073808416
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2022-08-21 03:26:03 +0000

    チェックディジットを求めるタイプとチェックディジットを求めたい値を入力する

--- app/src/checkDigit.ts
+++ app/src/checkDigit.ts
@@ -3,7 +3,11 @@ import { Mod10 } from "./calcFunc/mod10"
 export class CheckDigit {
     mod10 = () => {
         const mod10 = new Mod10
-        return mod10.mod10w2Split(589325)
-        return 'モジュラス10'
+        // return mod10.mod10w2Split(589325)
+        return mod10.mod10w2(5423589)
+    }
+
+    getCd = (type: number, value: number): string => {
+        return ''
     }
 }
